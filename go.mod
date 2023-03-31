2a3386fe0ac86a73c670da518f1c2e9b136d1de9
kushan <kuspan@mybudget.com.au>
2023-03-31 23:24:20 +1030

    packaging :package: configure add :heavy_plus_sign:

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module article-dispatcher
 
 go 1.18
+
+require (
+	github.com/olekukonko/tablewriter v0.0.5
+	gopkg.in/oleiade/reflections.v1 v1.0.0
+)
+
+require (
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/oleiade/reflections v1.0.1 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
+)
---
+++ internal/pkg/configs/configs.go
@@ -0,0 +1,69 @@
+package configs
+
+import (
+	"fmt"
+	"github.com/olekukonko/tablewriter"
+	"gopkg.in/oleiade/reflections.v1"
+	"log"
+	"os"
+)
+
+type Configure interface {
+	Register() error
+}
+type Validator interface {
+	Validate() error
+}
+
+type Printer interface {
+	Print() interface{}
+}
+
+// Load register,validate and print all the given configurations
+func Load(configs ...Configure) error {
+	for _, c := range configs {
+		err := c.Register()
+		if err != nil {
+			return err
+		}
+
+		v, ok := c.(Validator)
+		if ok {
+			err = v.Validate()
+			if err != nil {
+				return err
+			}
+		}
+
+		p, ok := c.(Printer)
+		if ok {
+			printTable(p)
+		}
+	}
+	return nil
+}
+
+// printTable log table writer
+func printTable(p Printer) {
+	table := tablewriter.NewWriter(os.Stdout)
+
+	var data [][]string
+
+	pr := p.Print()
+	var fields []string
+	fields, _ = reflections.Fields(pr)
+
+	for _, field := range fields {
+		value, err := reflections.GetField(pr, field)
+		if err != nil {
+			log.Printf("error printing the goconf table %s", err)
+		}
+		data = append(data, []string{field, fmt.Sprint(value)})
+	}
+
+	table.SetHeader([]string{"Config", "Value"})
+	table.AppendBulk(data)
+	table.SetAlignment(tablewriter.ALIGN_LEFT)
+
+	table.Render()
+}
